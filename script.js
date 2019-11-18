dff6e29de70b288cbeae38b8dcd85210906d93b3
Mattiassonjesper <57625574+Mattiassonjesper@users.noreply.github.com>
2019-11-18 19:21:27 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+function check(){
+
+	var question1 = document.quiz.question1.value;
+	var question2 = document.quiz.question2.value;
+	var question3 = document.quiz.question3.value;
+	var correct = 0;
+
+
+	if (question1 == "Stockholm") {
+		correct++;
+}
+	if (question2 == "Copenhagen") {
+		correct++;
+}	
+	if (question3 == "Helsinki") {
+		correct++;
+	}
+	
+	var pictures = ["img/pg1.jpg", "img/pg2.jpg", "img/pg3.jpg"];
+	var messages = ["Great job!", "That's just okay", "You really need to do better"];
+	var score;
+
+	if (correct == 0) {
+		score = 2;
+	}
+
+	if (correct > 0 && correct < 3) {
+		score = 1;
+	}
+
+	if (correct == 3) {
+		score = 0;
+	}
+
+	document.getElementById("after_submit").style.visibility = "visible";
+
+	document.getElementById("message").innerHTML = messages[score];
+	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+	document.getElementById("picture").src = pictures[score];
+	}
